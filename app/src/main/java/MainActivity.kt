56c0178f4c7d8b83d1b479cd63965235786a0d6d
Conastin <anqi.wang@hanthink.com>
2024-01-06 21:59:35 +0800

    Initial commit

---
+++ app/src/main/java/MainActivity.kt
@@ -0,0 +1,13 @@
+package im.see.again
+
+import android.os.Bundle
+import com.highcapable.betterandroid.ui.component.activity.AppBindingActivity
+import im.see.again.databinding.ActivityMainBinding
+
+class MainActivity : AppBindingActivity<ActivityMainBinding>() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO Start your coding here.
+    }
+}
