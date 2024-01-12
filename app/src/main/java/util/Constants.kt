cb8654d7c7e321cdb5e35d07ce99687ab101cf88
Conastin <anqi.wang@hanthink.com>
2024-01-12 22:54:14 +0800

    HistoryMyself type类型声明

---
+++ app/src/main/java/util/Constants.kt
@@ -0,0 +1,12 @@
+package im.see.again.util
+
+class Constants {
+    companion object {
+        /**
+         * 数据类型声明
+         */
+        const val typeLocked = 0   // 锁屏
+        const val typeBattery = 1  // 电量
+        const val typeNetwork = 2  // 网络
+    }
+}
